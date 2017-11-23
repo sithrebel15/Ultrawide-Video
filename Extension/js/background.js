e26d042fc577fef056b037899e5a3f3f79556643
sithrebel15 <anthonycorrado2@gmail.com>
2017-11-23 02:48:29 -0800

    Initial Commit
    Initial Commit

--- Extension/js/background.js
+++ Extension/js/background.js
@@ -4,16 +4,17 @@ chrome.runtime.onInstalled.addListener(function(details) {
 		chrome.notifications.create(null, {
 			type:'basic',
 			iconUrl:"images/icon.png",
-			title:"Ultrawide Video updated",
+			title:"Ultra Wide Video updated",
 			message:"Update log: Version "+ver+" added FORCE modes for 4:3 videos on 16:9 screens; Removed console logs; multiscreen stability",
 		}, function(updateNotificationId) {
 			chrome.notifications.onClicked.addListener(function(notificationId) {
 				if(notificationId === updateNotificationId) {
-					chrome.tabs.create({url:"https://github.com/wltrsjames/Ultrawide-Video"});
+					chrome.tabs.create({url:"https://github.com/sithrebel15/Ultra wide-Video"});
 					chrome.notifications.clear(updateNotificationId);
 				}
 			});
 		});
 	}
 	chrome.storage.local.set({'extensionMode':0}); 
+});
 });
